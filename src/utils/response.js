23f6d81df9fe2e79854aa8d96b6fdf34ff224454
IbnuRoi <ibnuroihan123@gmail.com>
2026-06-20 12:22:43 +0700

    add response format

---
+++ src/utils/response.js
@@ -0,0 +1,6 @@
+export const response = (status, data, message, res, meta = null) => {
+    const responseBody = data ? { status, data, ...(meta && { meta }), message } :
+    { status, message}
+
+    res.status(status).json(responseBody)
+}
